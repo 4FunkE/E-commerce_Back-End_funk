d3624e6b8f2a15c5b0f7085afb76d456b83e4c00
4FunkE <4FunkEcodes@gmail.com>
2023-08-08 22:01:59 -0400

    created producttag model and associations in indexjs

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,18 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-// Define a Category as having one Product to create a foreign key in the `Product` table
-Category.hasOne(Product, {
+Product.belongsTo(Category, {
   foreignKey: 'category_id',
-  // When we delete a category it will also delete the associated product.
-  onDelete: 'CASCADE',
 });
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
